0a73465db53412091a22df46ecb3b9b291661de4
Amit1985 <amit.pani100@gmail.com>
2018-01-10 16:31:18 +0530

    T

---
+++ src/test/java/io/springbootquickstart/cloud/CloudTestApiApplication.java
@@ -0,0 +1,12 @@
+package io.springbootquickstart.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudTestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudTestApiApplication.class, args);
+	}
+}
